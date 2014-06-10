312628eb53ba849ac1ffee710b2b6e01b0572fd9
andrefurlan <andrefurlan@gmail.com>
2014-06-09 19:20:13 -0700

    more persistency stuff

--- src/com/ubc/cpsc310/vancouverparking/server/TimeInEffect.java
+++ src/com/ubc/cpsc310/vancouverparking/server/TimeInEffect.java
@@ -1,5 +1,25 @@
 package com.ubc.cpsc310.vancouverparking.server;
 
-public class Time {
+import com.google.appengine.api.datastore.Key;
 
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable
+public class TimeInEffect {
+	@PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Key key;
+	@Persistent
+	private double start;
+	@Persistent
+	private double end;
+	
+	public TimeInEffect(double s, double e){
+		this.start = s;
+		this.end = e;
+	}
+	
 }
--- src/com/ubc/cpsc310/vancouverparking/server/TimeLimit.java
+++ src/com/ubc/cpsc310/vancouverparking/server/TimeLimit.java
@@ -1,5 +1,23 @@
 package com.ubc.cpsc310.vancouverparking.server;
 
-public class TimeLimit {
+import com.google.appengine.api.datastore.Key;
 
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable
+public class TimeLimit {
+	@PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Key key;
+	@Persistent
+	private double hour;
+	
+	public TimeLimit(double s){
+		this.hour = s;
+	}
+	
 }
+
